c5218336872118ccc77703ada839e1c7ecf15899
Shiramiki <mbeizacokila@gmail.com>
2023-07-03 20:16:35 +0300

    My  first C calculator Draft 1

---
+++ My first Calculator.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+ /*
+    This is a program that accepts integers from the user and performs mathematical calculations on them.  
+ */
+
+int main()
+{
+    int num =0, summ= 0, product = 0, difference =  0, quotient = 0, current = 0;
+
+    int addition(num){
+        summ +=num;
+
+        return summ;
+        
+    }
+
+    int multiplication(num){
+        product *=num;
+
+        return product;
+    }
+
+    int subtraction(num){
+        difference -= num;
+
+        return difference;
+    }
+
+    int division(num, ){
+        quotient /= num;
+
+        return quotient;
+    }
+
+    scanf("%d", &num);
+    
+    
+    
+    
+
+    return 0;
+
+}
